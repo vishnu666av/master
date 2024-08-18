dedd10c3ce524cebf1c13c3b526ca9ac076b9a9e
Rakesh Meena <rkumar.meena2012@gmail.com>
2024-08-18 18:36:07 -0400

    Switched from Dagger to Hilt for more streamlined and Android-specific DI

--- app/src/main/java/com/example/otchallenge/MainActivity.kt
+++ app/src/main/java/com/example/otchallenge/MainActivity.kt
@@ -5,11 +5,12 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
 	override fun onCreate(savedInstanceState: Bundle?) {
-		(application as MyApplication).appComponent.inject(this)
 		super.onCreate(savedInstanceState)
 		enableEdgeToEdge()
 		setContentView(R.layout.activity_main)
