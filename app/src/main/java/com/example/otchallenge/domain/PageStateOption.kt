413600d72cef4d6acdbd9ea5506bbf8369e947ec
Rakesh Meena <rkumar.meena2012@gmail.com>
2024-08-19 11:55:19 -0400

    Defined PageState for Loading, Success, Empty, and Error cases

---
+++ app/src/main/java/com/example/otchallenge/domain/PageStateOption.kt
@@ -0,0 +1,11 @@
+package com.example.otchallenge.domain
+
+/**
+ * Enum class for different states of a page.
+ */
+enum class PageStateOption {
+    DEFAULT,
+    ERROR,
+    LOADING,
+    EMPTY
+}
