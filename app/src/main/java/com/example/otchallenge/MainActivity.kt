86cb264bfd078bfc4fb59b3c760b736b5ae6c9cd
ridgetech <navdeepgrewal1988@gmail.com>
2024-09-03 13:48:32 -0700

    Initial commit with base structure

--- app/src/main/java/com/example/otchallenge/MainActivity.kt
+++ app/src/main/java/com/example/otchallenge/MainActivity.kt
@@ -9,7 +9,6 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
 
 	override fun onCreate(savedInstanceState: Bundle?) {
-		(application as MyApplication).appComponent.inject(this)
 		super.onCreate(savedInstanceState)
 		enableEdgeToEdge()
 		setContentView(R.layout.activity_main)
